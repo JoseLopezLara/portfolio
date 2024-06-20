9081b13fe0606fce0ae27c5f6ca25907016cf7da
JoseLopezLara <jose.lopez.lara.cto@gmail.com>
2024-06-20 02:30:54 -0600

    Production V1

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+
+module.exports = (env, argv) => {
+  const {mode} = argv
+  const isProduction = mode  === 'production' 
+  
+  return {
+    entry: path.resolve(__dirname, 'src/js/index.js'),
+    output: {
+        filename: isProduction
+        ? '[name].[contenthash].js'
+        : 'main.js',
+        path: path.resolve(__dirname, 'build'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: path.resolve(__dirname, 'src/index.html'), 
+          filename: 'index.html', 
+          inject: 'body',
+        }),
+        new CopyPlugin({
+          patterns: [
+            { from: 'src/assets', to: 'assets' },
+          ],
+        }),
+    ],
+    devServer: {
+        open: true,
+        port: 3001,
+        compress: true
+    },
+    module: {
+      rules: [
+          {
+              test: /\.(png|jpe?g|gif|svg)$/i,
+              type: 'asset/resource',
+          },
+      ],
+    },
+  }
+}
