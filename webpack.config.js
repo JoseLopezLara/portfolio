94260e922b6d635955138ca592fc085397f8a301
JoseLopezLara <jose.lopez.lara.cto@gmail.com>
2025-01-29 01:12:40 -0600

    Portfolio updated to 19-01-2025

--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,44 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require("copy-webpack-plugin");
 
-
 module.exports = (env, argv) => {
   const {mode} = argv
-  const isProduction = mode  === 'production' 
+  const isProduction = mode === 'production' 
   
   return {
     entry: path.resolve(__dirname, 'src/js/index.js'),
     output: {
-        filename: isProduction
+      filename: isProduction
         ? '[name].[contenthash].js'
         : 'main.js',
-        path: path.resolve(__dirname, 'build'),
+      path: path.resolve(__dirname, 'build'),
     },
     plugins: [
-        new HtmlWebpackPlugin({
-          template: path.resolve(__dirname, 'src/index.html'), 
-          filename: 'index.html', 
-          inject: 'body',
-        }),
-        new CopyPlugin({
-          patterns: [
-            { from: 'src/assets', to: 'assets' },
-          ],
-        }),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'src/index.html'),
+        filename: 'index.html',
+        inject: 'body', 
+      }),
+      new CopyPlugin({
+        patterns: [
+          { from: 'src/assets', to: 'assets' },
+        ],
+      }),
     ],
     devServer: {
-        open: true,
-        port: 3001,
-        compress: true
+      open: true,
+      port: 3001,
+      compress: true,
+      hot: true, // Enable HMR
+      watchFiles: ['src/**/*'], // Watch all files in src directory
+      liveReload: true // Enable live reload
     },
     module: {
       rules: [
-          {
-              test: /\.(png|jpe?g|gif|svg)$/i,
-              type: 'asset/resource',
-          },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          type: 'asset/resource',
+        },
       ],
     },
   }
